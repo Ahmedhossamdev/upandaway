02f4297f80a176734493082e206e8ea11bcda856
Ahmed hossam <you@example.com>
2023-05-14 22:41:59 +0300

    v2

--- server/routes/tours.js
+++ server/routes/tours.js
@@ -1,19 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const {
-
     Tours,
     showDetails,
     search,
-
-
  } = require('../controllers/packagesController');
-const {
 
-} = require('../controllers/detailsController');
 // TODO , IS LOGGED IN ?
 router.get('/packages', Tours);
-//router.get('/details' , Tours);
 router.get('/details/:id', showDetails);
 router.get('/results', search );
 
